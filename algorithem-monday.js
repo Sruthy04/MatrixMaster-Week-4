49d71ea08177ac62e54d82dbce247acbe6251f22
Sruthy <Sruthysusan04@gmail.com>
2024-03-11 17:34:39 +0100

    Game Of 3S

---
+++ algorithem-monday.js
@@ -0,0 +1,30 @@
+// Game of 3s.
+
+// define the function takes number as a parameter.
+// while loop until number is bigger than 1.
+// if condition to check if the number divisible by 3.
+// true divide by 3.
+// false check the remainder is 1.
+// if it's true, subtract 1 and divide by 3.
+// false add 1 and divide by 3.
+// return the number.
+
+function GameOf3s(number) {
+    while (number > 1){
+     if (number % 3 == 0){
+        console.log(number,0);
+        number/=3;
+     }else if (number % 3 == 1){
+        console.log(number,-1);
+        number-=1;
+        number/=3;
+        
+     }else{
+        console.log(number,+1)
+        number+=1
+        number/= 3  
+     }
+    } 
+    return number;
+}
+console.log(GameOf3s(100));
